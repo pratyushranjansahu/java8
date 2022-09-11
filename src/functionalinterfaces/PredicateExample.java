eca1e8155406ba79aaa2e7378071292b2dae1a8f
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-09-11 21:48:03 +0530

    added

--- src/functionalinterfaces/PredicateExample.java
+++ src/functionalinterfaces/PredicateExample.java
@@ -1,2 +1,30 @@
-package functionalinterfaces;public class PredicateExample {
+package functionalinterfaces;
+
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    static Predicate<Integer> p1 = i -> i % 2 == 0;
+    static Predicate<Integer> p2 = i -> i % 5 == 0;
+
+    public static void predicateAnd() {
+        System.out.println("predicateAnd");
+        System.out.println(p1.and(p2).test(9));//false
+    }
+
+    public static void predicateOr() {
+        System.out.println("predicateOr");
+        System.out.println(p1.or(p2).test(10));
+        System.out.println(p1.or(p2).test(8));
+    }
+
+    public static void predicateNegate() {
+        System.out.println("predicateNegate");
+        System.out.println(p1.or(p2).negate().test(10));//false
+    }
+    public static void main(String[] args) {
+       // System.out.println(p1.test(10));
+        predicateAnd();
+        predicateOr();
+        predicateNegate();
+    }
 }
--- src/functionalinterfaces/PredicateStudentExample.java
+++ src/functionalinterfaces/PredicateStudentExample.java
@@ -1,2 +1,50 @@
-package functionalinterfaces;public class PredicateStudentExample {
+package functionalinterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+public class PredicateStudentExample {
+    static Predicate<Student> p1 = (s) -> s.getGradeLevel() >= 3;
+    static Predicate<Student> p2 = (s) -> s.getGpa() >= 3.9;
+
+    public static void filterStudentByGradeLevel(){
+        System.out.println("filterStudentByGradeLevel");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(student -> {
+            if(p1.test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
+    public static void filterStudentByGpa(){
+        System.out.println("filterStudentByGpa");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(student -> {
+            if(p2.test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+
+    public static void filterStudents(){
+        System.out.println("filterStudents");
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(student -> {
+            if(p1.or(p2).test(student)){
+                System.out.println(student);
+            }
+        });
+    }
+    public static void main(String[] args) {
+        filterStudentByGradeLevel();
+        filterStudentByGpa();
+        filterStudents();
+    }
 }
