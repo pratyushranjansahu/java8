ce85fed1880de50ec8b62764fa0803535875f056
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-01 15:17:09 +0530

    Map reduce example

--- src/streams/StudentMapReduceExample.java
+++ src/streams/StudentMapReduceExample.java
@@ -1,2 +1,19 @@
-package streams;public class StudentMapReduceExample {
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+public class StudentMapReduceExample {
+    private static int getNumberOfNoteBooks(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .filter(student -> student.getGradeLevel() >= 3)
+                .filter(student -> student.getGender().equals("female"))
+                .map(Student::getNoteBooks)
+                //.reduce(0,(a,b)-> a+b);
+                .reduce(0,Integer::sum);
+    }
+    public static void main(String[] args) {
+        System.out.println(getNumberOfNoteBooks());
+    }
 }
