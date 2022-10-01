efa5eb06e8d703a761867d3b2db7c78e05968cfe
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-01 14:35:49 +0530

    Redeuce for Student Db

--- src/streams/StreamReduceExample.java
+++ src/streams/StreamReduceExample.java
@@ -1,11 +1,15 @@
 package streams;
 
+import data.Student;
+import data.StudentDataBase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class StreamReduceExample {
+
     private static int performMultiplication(List<Integer> intergerList) {
         return intergerList.stream()
                 .reduce(1,(a,b) -> a * b);
@@ -14,6 +18,12 @@ public class StreamReduceExample {
     private static Optional<Integer> performMultiplicationWithoutIdentity(List<Integer> intergerList) {
         return intergerList.stream()
                 .reduce((a,b) -> a * b);
+    }
+
+    private static Optional<Student> getHighestStudentGPA() {
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .reduce((s1,s2) -> s1.getGpa() > s2.getGpa() ? s1 : s2);
     }
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1,3,5,7);
@@ -24,6 +34,11 @@ public class StreamReduceExample {
         System.out.println(performMultiplicationWithoutIdentity(integers1).isPresent());
         if(performMultiplicationWithoutIdentity(integers1).isPresent()){
             System.out.println(performMultiplicationWithoutIdentity(integers1).get());
+        }
+
+        Optional<Student> optionalStudent = getHighestStudentGPA();
+        if(optionalStudent.isPresent()){
+            System.out.println(optionalStudent.get());
         }
     }
 }
