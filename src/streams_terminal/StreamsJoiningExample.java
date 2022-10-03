eb8ae14501413887461c7fcde55d70983bd843c9
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-03 06:53:43 +0530

    Joining Collector Example

---
+++ src/streams_terminal/StreamsJoiningExample.java
@@ -0,0 +1,36 @@
+package streams_terminal;
+
+import data.Student;
+import data.StudentDataBase;
+
+import static java.util.stream.Collectors.joining;
+
+public class StreamsJoiningExample {
+
+    private static String joining_1(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(joining());
+    }
+
+    private static String joining_2(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(joining(","));
+    }
+
+    private static String joining_3(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(joining(",","(",")"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("joining_1 : "+joining_1());
+        System.out.println("joining_2 : "+joining_2());
+        System.out.println("joining_3 : "+joining_3());
+    }
+}
