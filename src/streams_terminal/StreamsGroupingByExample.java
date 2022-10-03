c12e649d5c25e12bf13a5079a1c04c1b83fd7d04
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-03 08:13:46 +0530

    Grouping By Level 2

--- src/streams_terminal/StreamsGroupingByExample.java
+++ src/streams_terminal/StreamsGroupingByExample.java
@@ -22,10 +22,38 @@ public class StreamsGroupingByExample {
                 .collect(Collectors.groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE"));
         System.out.println(studentMap);
     }
+
+    private static void twoLevelGrouping_1(){
+        Map<Integer,Map<String, List<Student>>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,Collectors.groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE")));
+        System.out.println(studentMap);
+    }
+
+    private static void twoLevelGrouping_2(){
+        Map<Integer,Integer> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,Collectors.summingInt(Student::getNoteBooks)));
+        System.out.println(studentMap);
+    }
+
+    private static void twoLevelGrouping_2a(){
+        Map<String,Integer> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getName,Collectors.summingInt(Student::getNoteBooks)));
+        System.out.println(studentMap);
+    }
+
     public static void main(String[] args) {
         System.out.println("Grouping By");
         groupingByGender();
         System.out.println("Customize Grouping By");
         customizedGroupingBy();
+        System.out.println("twoLevelGrouping_1 Grouping By");
+        twoLevelGrouping_1();
+        System.out.println("twoLevelGrouping_2 Grouping By");
+        twoLevelGrouping_2();
+        System.out.println("twoLevelGrouping_2a Grouping By");
+        twoLevelGrouping_2a();
     }
 }
