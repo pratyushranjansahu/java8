7d628d192082a7de343625bb8d54cef9fb97dc29
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2022-10-04 06:45:46 +0530

    performance of sequential and parallel

--- src/parallelstream/ParallelStreamExample.java
+++ src/parallelstream/ParallelStreamExample.java
@@ -1,9 +1,18 @@
 package parallelstream;
 
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class ParallelStreamExample {
 
+    private static long checkPerformance(Supplier<Integer> supplier, int numberOfTimes){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numberOfTimes; i++) {
+            supplier.get();
+        }
+        long end = System.currentTimeMillis();
+        return (end - start);
+    }
     private static int sequentialStream(){
         return IntStream.rangeClosed(1,100000)
                 .sum();
@@ -15,7 +24,7 @@ public class ParallelStreamExample {
                 .sum();
     }
     public static void main(String[] args) {
-        System.out.println(sequentialStream());
-        System.out.println(parallelStream());
+        System.out.println("Sequential Stream Result : "+checkPerformance(ParallelStreamExample::sequentialStream,20));
+        System.out.println("Parallel Stream Result : "+checkPerformance(ParallelStreamExample::parallelStream,20));
     }
 }
